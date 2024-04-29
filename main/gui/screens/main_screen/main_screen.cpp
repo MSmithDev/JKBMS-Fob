fe5ed2c51b7cb498d036e228cbce5156f055635f
Matt <mattdsmith228@gmail.com>
2024-04-29 01:03:24 -0400

    added precent charged to raw

--- main/gui/screens/main_screen/main_screen.cpp
+++ main/gui/screens/main_screen/main_screen.cpp
@@ -30,12 +30,14 @@ void main_screen(LGFX_Sprite canvas, GlobalState *globalState, JKBMSData *jkData
                     std::string avgCellV = "Avg Cell V: " + floatToString(jkData->avgCellVoltage, 3);
                     std::string cycleAh = "Cycle Ah: " + floatToString(jkData->cycleAh, 2);
                     std::string cycles = "Cycles: " + std::to_string(jkData->cycleCount);
+                    std::string percent = "Percent: " + std::to_string(jkData->packPercentage) + "%";
 
                     canvas.drawString(voltage.c_str(), 40, 40);
                     canvas.drawString(current.c_str(), 40, 55);
                     canvas.drawString(avgCellV.c_str(), 40, 70);
                     canvas.drawString(cycleAh.c_str(), 40, 85);
                     canvas.drawString(cycles.c_str(), 40, 100);
+                    canvas.drawString(percent.c_str(), 40, 115);
                     
                 
                 }
