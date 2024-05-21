178149a1d17eb5dc3fbcc79e1b10b8818581afa2
Matt <43940868+MSmithDev@users.noreply.github.com>
2024-05-21 11:15:19 -0400

    Simplified page nav

--- main/tasks/gui_task.cpp
+++ main/tasks/gui_task.cpp
@@ -1,15 +1,12 @@
 #include "gui_task.h"
 #include "LovyanGFX.hpp"
-#include <string>
 #include "gui/LGFX_Config.hpp"
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
-#include <iostream>
-#include <iomanip>
-#include <sstream>
 #include "driver/gpio.h"
+
 // Widgets
 #include "gui/widgets/widgets.hpp"
 
@@ -23,7 +20,7 @@
 #include "gui/screens/control_screen/control_screen.hpp"
 #include "gui/screens/info_screen/info_screen.hpp"
 
-// helpers
+// Helpers
 #include "helpers/jkbms.h"
 #include "helpers/utils.h"
 
@@ -41,20 +38,56 @@ extern QueueHandle_t bleConnection;
 extern QueueHandle_t ble_data_queue;
 extern QueueHandle_t bleScan_data_queue;
 
+
+void handle_page_navigation()
+{
+    if (globalState.upKey)
+    {
+        switch(globalState.CurrentPage)
+        {
+            case 0:
+                globalState.bleConnected ? globalState.CurrentPage-- : globalState.CurrentPage = 3;
+                break;
+
+            case 3:    
+                globalState.bleConnected ? globalState.CurrentPage-- : globalState.CurrentPage = 0;
+                break;
+        }
+        if (globalState.CurrentPage < 0) globalState.CurrentPage = 3;
+        
+    }
+    if (globalState.downKey)
+    {
+        switch(globalState.CurrentPage)
+        {
+            case 0:
+                globalState.bleConnected ? globalState.CurrentPage++ : globalState.CurrentPage = 3;
+                break;
+
+            case 3:    
+                globalState.bleConnected ? globalState.CurrentPage++ : globalState.CurrentPage = 0;
+                break;
+        }
+        if (globalState.CurrentPage > 3) globalState.CurrentPage = 0;
+    }
+}
+
+
 void gui_task(void *pvParameters)
 {
-
     ble_data_queue = xQueueCreate(5, sizeof(BLEControl));
 
-    // Initialize 3 buttons
+    // Initialize buttons individually
     // Button UP
     gpio_pad_select_gpio(GPIO_NUM_0);
     gpio_set_direction(GPIO_NUM_0, GPIO_MODE_INPUT);
     gpio_set_pull_mode(GPIO_NUM_0, GPIO_PULLUP_ONLY);
+
     // Button SELECT
     gpio_pad_select_gpio(GPIO_NUM_1);
     gpio_set_direction(GPIO_NUM_1, GPIO_MODE_INPUT);
     gpio_set_pull_mode(GPIO_NUM_1, GPIO_PULLUP_PULLDOWN);
+
     // Button DOWN
     gpio_pad_select_gpio(GPIO_NUM_2);
     gpio_set_direction(GPIO_NUM_2, GPIO_MODE_INPUT);
@@ -66,26 +99,22 @@ void gui_task(void *pvParameters)
 
     // Splash Screen
     display.pushImage(0, 0, 240, 135, image_data_splash);
-
     vTaskDelay(pdMS_TO_TICKS(1000));
 
-    // Create a sprite for the background
-    LGFX_Sprite bgSprite(&display);
-    bgSprite.createSprite(240, 135);
+    LGFX_Sprite canvas(&display);
+    canvas.createSprite(240, 135);
 
-    // Screens
-
-    // Variable to remember the last state of the button
-    bool lasUPKeyState = true;
-    bool lasDownKeyState = true;
-    bool lasSelectKeyState = true;
+    // Variable to remember the last state of the buttons
+    bool lastUPKeyState = true;
+    bool lastDownKeyState = true;
+    bool lastSelectKeyState = true;
 
     globalState.CurrentPage = 0; // Main Page
 
     // Main GUI Loop
     while (1)
     {
-
+        // Handle BLE connection and JKBMS data
         if (xQueueReceive(bleConnection, &globalState.bleConnected, (TickType_t)5))
         {
             ESP_LOGI(TAG, "Got BLE Connection Data %i", globalState.bleConnected);
@@ -93,7 +122,7 @@ void gui_task(void *pvParameters)
 
         if (xQueueReceive(jkbms_data_queue, &jkbmsData, (TickType_t)5))
         {
-            // ESP_LOGI(TAG, "Got JKBMS Data");
+            // Handle JKBMS Data if needed
         }
 
         // Get the current state of the buttons
@@ -101,180 +130,46 @@ void gui_task(void *pvParameters)
         bool curDownKeyState = gpio_get_level(GPIO_NUM_2) == 1;
         bool curSelectKeyState = gpio_get_level(GPIO_NUM_1) == 0;
 
-        if (!lasUPKeyState && curUPKeyState)
-        {
-            globalState.upKey = true;
-        }
-        else
-        {
-            globalState.upKey = false;
-        }
-
-        if (!lasDownKeyState && curDownKeyState)
-        {
-            globalState.downKey = true;
-        }
-        else
-        {
-            globalState.downKey = false;
-        }
-
-        if (!lasSelectKeyState && curSelectKeyState)
-        {
-            globalState.selectKey = true;
-        }
-        else
-        {
-            globalState.selectKey = false;
-        }
+        globalState.upKey = !lastUPKeyState && curUPKeyState;
+        globalState.downKey = !lastDownKeyState && curDownKeyState;
+        globalState.selectKey = !lastSelectKeyState && curSelectKeyState;
 
         // Update last state to current state
-        lasUPKeyState = curUPKeyState;
-        lasDownKeyState = curDownKeyState;
-        lasSelectKeyState = curSelectKeyState;
+        lastUPKeyState = curUPKeyState;
+        lastDownKeyState = curDownKeyState;
+        lastSelectKeyState = curSelectKeyState;
 
-        bgSprite.setSwapBytes(true);
-        bgSprite.setColorDepth(16);
+        canvas.setSwapBytes(true);
+        canvas.setColorDepth(16);
 
+        // Handle the current page
         switch (globalState.CurrentPage)
         {
-
-        case 0: // Main Page
-
-            // Update the current page based on the button press
-            if (globalState.upKey)
-            {
-                globalState.CurrentPage--;
-                if (globalState.CurrentPage < 0)
-                {
-                    globalState.CurrentPage = 3;
-                }
-            }
-            if (globalState.downKey)
-            {
-                // If BLE is not connected, skip the settings page
-                if (globalState.bleConnected)
-                {
-                    globalState.CurrentPage++;
-                }
-                else
-                {
-                    globalState.CurrentPage = 3;
-                }
-
-                if (globalState.CurrentPage > 3)
-                {
-                    globalState.CurrentPage = 0;
-                }
-            }
-
-            main_screen(bgSprite, &globalState, &jkbmsData);
-            UIWidgets::navBar(bgSprite, curUPKeyState, curSelectKeyState, curDownKeyState);
-            UIWidgets::statusBar(bgSprite, &globalState, &jkbmsData);
-
-            bgSprite.pushSprite(0, 0);
-            break;
-
-        case 1: // Info Page
-
-            // Update the current page based on the button press
-            if (globalState.upKey)
-            {
-                globalState.CurrentPage--;
-                if (globalState.CurrentPage < 0)
-                {
-                    globalState.CurrentPage = 3;
-                }
-            }
-            if (globalState.downKey)
-            {
-                globalState.CurrentPage++;
-                if (globalState.CurrentPage > 3)
-                {
-                    globalState.CurrentPage = 0;
-                }
-            }
-
-            info_screen(bgSprite, &globalState, &jkbmsData);
-            UIWidgets::navBar(bgSprite, curUPKeyState, curSelectKeyState, curDownKeyState);
-            UIWidgets::statusBar(bgSprite, &globalState, &jkbmsData);
-            bgSprite.pushSprite(0, 0);
-            break;
-
-        case 2: // Control Page
-
-            if (globalState.inControl == false)
-            {
-                // Update the current page based on the button press
-                if (globalState.upKey)
-                {
-                    globalState.CurrentPage--;
-                    if (globalState.CurrentPage < 0)
-                    {
-                        globalState.CurrentPage = 0;
-                    }
-                }
-                if (globalState.downKey)
-                {
-                    globalState.CurrentPage++;
-                    if (globalState.CurrentPage > 3)
-                    {
-                        globalState.CurrentPage = 0;
-                    }
-                }
-            }
-
-            control_screen(bgSprite, &globalState, &jkbmsData);
-            UIWidgets::navBar(bgSprite, curUPKeyState, curSelectKeyState, curDownKeyState);
-            UIWidgets::statusBar(bgSprite, &globalState, &jkbmsData);
-            bgSprite.pushSprite(0, 0);
-            break;
-
-        case 3: // Settings Page
-
-            // Update the current page based on the button press
-            if (globalState.settingsPage == 0)
-            {
-                if (globalState.upKey)
-                {
-                    if (globalState.bleConnected)
-                    {
-                        globalState.CurrentPage--;
-                    }
-                    else
-                    {
-                        globalState.CurrentPage = 0;
-                    }
-                    if (globalState.CurrentPage < 0)
-                    {
-                        globalState.CurrentPage = 3;
-                    }
-                }
-                if (globalState.downKey)
-                {
-                    if (globalState.bleConnected)
-                    {
-                        globalState.CurrentPage++;
-                    }
-                    else
-                    {
-                        globalState.CurrentPage = 0;
-                    }
-                    if (globalState.CurrentPage > 3)
-                    {
-                        globalState.CurrentPage = 0;
-                    }
-                }
-            }
-
-            settings_screen(bgSprite, &globalState);
-            UIWidgets::navBar(bgSprite, curUPKeyState, curSelectKeyState, curDownKeyState);
-            UIWidgets::statusBar(bgSprite, &globalState, &jkbmsData);
-
-            bgSprite.pushSprite(0, 0);
-            break;
+            case 0: // Main Page
+                handle_page_navigation();
+                main_screen(canvas, &globalState, &jkbmsData);
+                break;
+            case 1: // Info Page
+                // TODO: Make select key toggle between info screens
+                handle_page_navigation();
+                info_screen(canvas, &globalState, &jkbmsData);
+                break;
+            case 2: // Control Page
+                if (!globalState.inControl) handle_page_navigation();
+                control_screen(canvas, &globalState, &jkbmsData);
+                break;
+            case 3: // Settings Page
+                if (globalState.settingsPage == 0) handle_page_navigation();
+                settings_screen(canvas, &globalState);
+                break;
         }
+
+        UIWidgets::navBar(canvas, curUPKeyState, curSelectKeyState, curDownKeyState);
+        UIWidgets::statusBar(canvas, &globalState, &jkbmsData);
+        canvas.pushSprite(0, 0);
 
         vTaskDelay(pdMS_TO_TICKS(10));
     }
 }
+
+
